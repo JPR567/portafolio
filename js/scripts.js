6e2a9e32566f0f322eb73b6e69db65dac6713385
Juan Pablo Rivadeneira <jprivadeneiral@estud.usfq.edu.ec>
2024-11-05 23:38:11 -0500

    Resuelto conflicto en index.html

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const welcomeSection = document.getElementById("welcome-section");
+    
+    // Cuando el mouse entre en la sección de bienvenida
+    welcomeSection.addEventListener("mouseenter", function () {
+        welcomeSection.classList.add("fade-out"); // Agregar clase para desvanecer
+    });
+
+    // Cuando la transición termine, ocultar la sección
+    welcomeSection.addEventListener("transitionend", function () {
+        welcomeSection.style.display = "none"; // Ocultar la sección completamente
+    });
+});
